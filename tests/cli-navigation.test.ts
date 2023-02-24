a32cbf4840a65953f357549ff725b56712a456c6
Benjamin Jesuiter <bjesuiter@gmail.com>
2023-02-24 23:17:57 +0100

    add cli-navigation test

---
+++ tests/cli-navigation.test.ts
@@ -0,0 +1,14 @@
+import { tty } from '../deps.ts';
+import { CLEAR_LINE, ESC } from "../src/types/AnsiEscapeCodes.ts";
+
+Deno.test(`Test CLI navigation Skills`, () => {
+
+    tty.writeSync(`Hello World 1 \n`, Deno.stdout);
+    tty.writeSync(`Hello World 2 \n`, Deno.stdout);
+    tty.writeSync(`Hello World 3 \n`, Deno.stdout);
+    tty.writeSync(`Hello World 4 \n`, Deno.stdout);
+
+    const moveDistance = 1;
+    const msg = `${ESC}${moveDistance}F${ESC}${CLEAR_LINE}HELLO BOB!\n${ESC}${moveDistance - 1}E`;
+    tty.writeSync(msg, Deno.stdout);
+})
