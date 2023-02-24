a8f7c309f74f3097707bd000497560d1c51a4b45
Benjamin Jesuiter <bjesuiter@gmail.com>
2023-02-24 22:36:36 +0100

    export tty lib as named export

--- deps.ts
+++ deps.ts
@@ -6,4 +6,4 @@ export {
 export { writeAllSync } from "https://deno.land/std@0.162.0/streams/conversion.ts";
 export { z } from "https://deno.land/x/zod@v3.20.5/mod.ts";
 export * from "https://esm.sh/@preact/signals-core@1.2.3";
-export * from "https://deno.land/x/tty@0.1.4/mod.ts"
+export * as tty from "https://deno.land/x/tty@0.1.4/mod.ts"
