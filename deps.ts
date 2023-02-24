3330ec3bc1d0290c3770b8a6e39fb9679dbf6a14
Benjamin Jesuiter <bjesuiter@gmail.com>
2023-02-24 22:35:51 +0100

    add preact signals and tty package

--- deps.ts
+++ deps.ts
@@ -5,4 +5,5 @@ export {
 } from "https://deno.land/std@0.162.0/fmt/colors.ts";
 export { writeAllSync } from "https://deno.land/std@0.162.0/streams/conversion.ts";
 export { z } from "https://deno.land/x/zod@v3.20.5/mod.ts";
-export { signal, effect } from "https://esm.sh/@preact/signals-core@1.2.3";
+export * from "https://esm.sh/@preact/signals-core@1.2.3";
+export * from "https://deno.land/x/tty@0.1.4/mod.ts"
