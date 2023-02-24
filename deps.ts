62dfa2749f9c70503fa1ed8501e0d6ff5538520d
Benjamin Jesuiter <bjesuiter@gmail.com>
2023-02-24 15:05:26 +0100

    fix zod dep

--- deps.ts
+++ deps.ts
@@ -4,4 +4,4 @@ export {
   stripColor,
 } from "https://deno.land/std@0.162.0/fmt/colors.ts";
 export { writeAllSync } from "https://deno.land/std@0.162.0/streams/conversion.ts";
-export { z } from "https://deno.land/x/zod@v3.20.5";
+export { z } from "https://deno.land/x/zod@v3.20.5/mod.ts";
