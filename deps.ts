510640c8f394fedd2f8ae2a30f86f32f7019add7
Benjamin Jesuiter <bjesuiter@gmail.com>
2023-02-24 18:14:13 +0100

    add preact signal core to simplify state management in MultiProgressBarWIP

--- deps.ts
+++ deps.ts
@@ -5,3 +5,4 @@ export {
 } from "https://deno.land/std@0.162.0/fmt/colors.ts";
 export { writeAllSync } from "https://deno.land/std@0.162.0/streams/conversion.ts";
 export { z } from "https://deno.land/x/zod@v3.20.5/mod.ts";
+export { signal, effect } from "https://esm.sh/@preact/signals-core@1.2.3";
