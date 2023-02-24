fd86337d1ac427733c674bc7e2e71e4fd711f88b
Benjamin Jesuiter <bjesuiter@gmail.com>
2023-02-24 15:12:35 +0100

    add comment to PrettyTimeOptions type

--- src/types/PrettyTimeOptions.ts
+++ src/types/PrettyTimeOptions.ts
@@ -6,4 +6,10 @@ export const PrettyTimeOptions = z.object({
   longFormat: z.boolean().optional(),
 });
 
+/**
+ * PrettyTime Options
+ * - withSpaces Whether to use spaces to separate times, `1d2h3m5s` or `1d 2h 3m 5s`, default false
+ * - toFixedVal value pass to toFixed for seconds, default 1
+ * - longFormat Whether to use a long format, default false, `1d2h3m5s` or `1days 2hours 3minutes 5seconds`
+ */
 export type PrettyTimeOptions = z.infer<typeof PrettyTimeOptions>;
