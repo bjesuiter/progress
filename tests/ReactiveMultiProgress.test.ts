6e549b1766279d1ff14a173f41ad8e75e57c70bc
Benjamin Jesuiter <bjesuiter@gmail.com>
2023-02-25 13:26:29 +0100

    make reactive multi progress test harder

--- tests/ReactiveMultiProgress.test.ts
+++ tests/ReactiveMultiProgress.test.ts
@@ -3,6 +3,7 @@ import { ReactiveMultiProgress } from '../src/ReactiveMultiProgress.ts';
 import { tty } from "../deps.ts"
 
 import {
+    map,
     timerSource,
 } from "./_testdeps.ts";
 
@@ -16,26 +17,26 @@ Deno.test(`Test`, async () => {
 
     const promise1 = instance.addProgressBar({
         label: 'Timer 1',
-        total: 100,
-        progressStream: timerSource({ maxEventCount: 100, intervalInMilliseconds: 100 })
+        total: 25,
+        progressStream: timerSource({ maxEventCount: 400, intervalInMilliseconds: 25 }).pipeThrough(map(count => count / 4))
     })
 
     const promise2 = instance.addProgressBar({
         label: 'Timer 2',
         total: 100,
-        progressStream: timerSource({ maxEventCount: 100, intervalInMilliseconds: 75 })
+        progressStream: timerSource({ maxEventCount: 100, intervalInMilliseconds: 100 })
     })
 
     const promise3 = instance.addProgressBar({
         label: 'Timer 3',
         total: 100,
-        progressStream: timerSource({ maxEventCount: 100, intervalInMilliseconds: 50 })
+        progressStream: timerSource({ maxEventCount: 100, intervalInMilliseconds: 100 })
     })
 
     const promise4 = instance.addProgressBar({
         label: 'Timer 4',
         total: 100,
-        progressStream: timerSource({ maxEventCount: 100, intervalInMilliseconds: 25 })
+        progressStream: timerSource({ maxEventCount: 100, intervalInMilliseconds: 100 })
     })
 
     await Promise.all([promise1, promise2, promise3, promise4])
