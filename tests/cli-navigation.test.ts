b087add678c0040c71146f8cb94362922d68f9ba
Benjamin Jesuiter <bjesuiter@gmail.com>
2023-02-25 11:48:51 +0100

    add test foro cli-naigation

--- tests/cli-navigation.test.ts
+++ tests/cli-navigation.test.ts
@@ -12,3 +12,18 @@ Deno.test(`Test CLI navigation Skills`, () => {
     const msg = `${ESC}${moveDistance}F${ESC}${CLEAR_LINE}HELLO BOB!\n${ESC}${moveDistance - 1}E`;
     tty.writeSync(msg, Deno.stdout);
 })
+
+Deno.test(`interactive navigation`, () => {
+
+    tty.writeSync(`${ESC}6n`, Deno.stdout);
+
+    tty.writeSync(`Hello World 1 \n`, Deno.stdout);
+    tty.writeSync(`Hello World 2 \n`, Deno.stdout);
+    tty.writeSync(`Hello World 3 \n`, Deno.stdout);
+    tty.writeSync(`Hello World 4 \n`, Deno.stdout);
+
+
+    const moveDistance = 1;
+    const msg = `${ESC}${moveDistance}F${ESC}${CLEAR_LINE}HELLO BOB!\n${ESC}${moveDistance - 1}E`;
+    tty.writeSync(msg, Deno.stdout);
+})
